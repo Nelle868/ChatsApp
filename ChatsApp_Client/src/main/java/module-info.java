de5fa87e09e7c059dd0356ce2eb718b45ee4e10f
Janelle <91690219+Nelle868@users.noreply.github.com>
2024-06-07 18:49:33 -0700

    Delete ChatsApp_Client directory

--- ChatsApp_Client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chatsapp_client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.chatsapp_client to javafx.fxml;
-    exports com.example.chatsapp_client;
-}
